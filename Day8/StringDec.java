65743f21b9251e101f906bf7c4233cb1009adf62
SirajSmd77 <siraj.mhd999@gmail.com>
2022-01-02 19:54:25 +0530

    RecursionPoweN/Gcd/StringsAsecding/StringDecending/ DSA-Assignment

---
+++ Day8/StringDec.java
@@ -0,0 +1,28 @@
+package Day8;
+
+public class StringDec {
+
+static final int MAX_CHAR = 26;
+	
+	public static void main(String[] args) {
+
+		sortString("geekstergivesusbestcoaching");
+		
+	}
+
+	static void sortString(String str) {
+		 
+        int characters[] = new int[MAX_CHAR];
+ 
+        for (char x : str.toCharArray()) {
+        	characters[x - 'a']++;
+        }
+ 
+        for (int i = 0; i < MAX_CHAR; i++) {
+            for (int j = 0; j <characters[i]; j++) {
+                System.out.print((char) (i + 'a'));
+            }
+        }
+	}
+
+}
