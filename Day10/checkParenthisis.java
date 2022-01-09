e2df7b2cf28b644422ac4dd0defb6fa0ce0792f7
SirajSmd77 <siraj.mhd999@gmail.com>
2022-01-09 19:58:53 +0530

    Week3/Stack/Queue/ DSA-Assignment

---
+++ Day10/checkParenthisis.java
@@ -0,0 +1,33 @@
+package Day10;
+
+public class checkParenthisis {
+
+	public static void main(String[] args) {
+		String s = "((){})";
+		
+		int top = -1;
+		char[] a = new char[s.length()];
+		
+		for(int i = 0;i<s.length();i++) {
+			if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+				top++;
+				a[top] = s.charAt(i);
+			}
+			else if((s.charAt(i) == ')' && a[top] == '(') || (s.charAt(i) == '}' && a[top] == '{') || 
+					(s.charAt(i) == ']' && a[top] == '[')) {
+				top--;
+			}
+			else {
+				continue;
+			}
+		}
+		
+		if(top == -1) {
+			System.out.println("Valid pair of peranthasis");
+		}
+		else {
+			System.out.println("Invalid pair of peranthasis");
+		}
+	}
+
+}
